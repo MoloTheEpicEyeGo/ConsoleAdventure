a88513aeb300a04f98b33879686a735a46d72a97
Travis Trinidad <molotheepiceyego@gmail.com>
2025-03-17 23:15:23 -0700

    - laying out classes

--- main.cpp
+++ main.cpp
@@ -26,3 +26,38 @@ public: //can be accessed anywhere
     }
 };
 
+//story class
+class Story {
+    //string description: the text representing what happens at this point in the game
+    //int eventnumber: a unique identifier for each event
+    //int lefteventnumber: the event number of the next event if the player chooses the left path
+    //int righteventnumber: the event number of the next event if the player chooses the right path
+};
+
+//gamedecisiontree class template
+template<typename T>
+class GameDecisionTree {
+    //method to load the story data from a file and create the tree
+    //method to traverse the decision tree based on player input
+};
+
+//binarytreestructure class
+class BinaryTreeStructure {
+    //method to implement one exception where multiple events may lead to the same outcome
+    //method to handle shared child nodes to the same memory location
+};
+
+//textbasedrpgwithexternalfileinput class
+class TextBasedRPGWithExternalFileInput {
+    //method to load the game story from a text file
+    //method to start the game by loading events into a binary tree and allow player decisions
+};
+
+//mainfunction class
+class MainFunction {
+    //method to load file function to load the story into the decision tree
+    //method to play game function to begin the game
+};
+
+
+
