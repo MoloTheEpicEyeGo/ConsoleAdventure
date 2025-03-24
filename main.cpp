44fe502d23266e87979af74bc1fc30366366e7e4
Travis Trinidad <molotheepiceyego@gmail.com>
2025-03-24 15:11:49 -0700

    - fixed classes so it matches github classroom

--- main.cpp
+++ main.cpp
@@ -1,63 +1,19 @@
 #include <iostream>
+#include <string>
+#include "GameDecisionTree.h"
 
-//Node Class
-template <typename T>
-class Node{ //template  meaning that it accepts any data type
+using namespace std;
+int main() {
+    GameDecisionTree<Story> myGame;
 
-public: //can be accessed anywhere
+    string filename = "story.txt";  // Specify the input file
+    char delimiter = '|';                // '|' is the delimiter, change if needed
 
-    T *value; //type t, pointer to "value"
+    // Load the story data from the file
+    myGame.loadStoryFromFile(filename, delimiter);
 
-    Node<T> * left; //node pointing to the left child
-    Node<T> * right; //node pointing to the right child
+    // Start the game
+    myGame.playGame();
 
-
-
-    Node(T *value) //makes a new node object
-    {
-        this->value = value; //stores the pointer to "value"
-        this->left = nullptr; //means that the node does not have a left child
-        this->right = nullptr; //means that the node does not have a right child
-    }
-
-    void print()
-    {
-        value->print();
-    }
-};
-
-//story class
-class Story {
-    //string description: the text representing what happens at this point in the game
-    //int eventnumber: a unique identifier for each event
-    //int lefteventnumber: the event number of the next event if the player chooses the left path
-    //int righteventnumber: the event number of the next event if the player chooses the right path
-};
-
-//gamedecisiontree class template
-template<typename T>
-class GameDecisionTree {
-    //method to load the story data from a file and create the tree
-    //method to traverse the decision tree based on player input
-};
-
-//binarytreestructure class
-class BinaryTreeStructure {
-    //method to implement one exception where multiple events may lead to the same outcome
-    //method to handle shared child nodes to the same memory location
-};
-
-//textbasedrpgwithexternalfileinput class
-class TextBasedRPGWithExternalFileInput {
-    //method to load the game story from a text file
-    //method to start the game by loading events into a binary tree and allow player decisions
-};
-
-//mainfunction class
-class MainFunction {
-    //method to load file function to load the story into the decision tree
-    //method to play game function to begin the game
-};
-
-
-
+    return 0;
+}
